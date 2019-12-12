c3089114cc22c5313143a2d92fa2ddd534048bdc
unknown <Admin@IHR80PBF201.continental.edu.pe>
2019-12-11 19:55:47 -0500

    El primer cambio en la aplicacion principal

---
+++ app/src/main/java/com/example/aromasapp/RecuperarContraseniaActivity.java
@@ -0,0 +1,14 @@
+package com.example.aromasapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecuperarContraseniaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar_contrasenia);
+    }
+}
